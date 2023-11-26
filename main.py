7cdac6cbb8cd9c3fd8777d0715d8f8a040d036af
mesasarah <102405106+mesasarah@users.noreply.github.com>
2023-11-26 17:41:25 +0530

    main.py

--- main.py
+++ main.py
@@ -1,44 +1,39 @@
 import streamlit as st
+import openpyxl
 
-# Student Details
-st.header("Student Details")
+# Initialize Excel workbook
+wb = openpyxl.Workbook()
+worksheet = wb.active
 
-name = st.text_input("Student Name:")
-branch = st.text_input("Branch:")
+# Set page title and subtitle
+st.title("Board of Community Development")
+st.subheader("Student Details")
 
-# University Selection
-universities = ["ANDHRA UNIVERSITY","Acharya Nagarjuna University", "sastry University", "Andhra University", "Damondaram Sanjivayya National Law University", "Dr. B.R. Ambedkar University","NTR Health University", "Darvidian University", "Gandhi Insitute of technology"]
-university = st.selectbox("University:", universities)
+# Create form elements for student details
+student_name = st.text_input(label="Student Name:")
+branch = st.text_input(label="Branch:")
+email = st.text_input(label="Email:")
 
-if university == "ANDHRA UNIVERSITY":
-    colleges = ["AMAL DEGREE COLLEGE", "MR COLLEGE FOR MEN", "GAYATRI VIDYA PARISHAD COLLEGE FOR DEGREE AND PG COURSES", "ST JOSEPHS DEGREE COLLEGE FOR WOMEN",""]
-    college = st.selectbox("College:", colleges)
-elif university == "Stanford University":
-    colleges = ["School of Humanities and Sciences", "School of Engineering", "Graduate School of Business"]
-    college = st.selectbox("College:", colleges)
-elif university == "Massachusetts Institute of Technology":
-    colleges = ["School of Science", "School of Engineering", "Sloan School of Management"]
-    college = st.selectbox("College:", colleges)
+# Dynamic university selection
+selected_university = st.selectbox(label="University:", options=["Select University", "University 1", "University 2"])
 
-# College Selection
-st.header("College Selection")
+# Dynamic college selection based on university choice
+if selected_university == "University 1":
+    college_options = ["College 1A", "College 1B"]
+elif selected_university == "University 2":
+    college_options = ["College 2A", "College 2B"]
+else:
+    college_options = []
 
-if college is not None:
-    st.write(f"You have selected {college}.")
+selected_college = st.selectbox(label="College:", options=college_options)
 
-    # Display additional information about the college
-    if college == "College of Letters and Science":
-        st.write("The College of Letters and Science is the largest of the colleges at UC Berkeley.")
-    elif college == "College of Engineering":
-        st.write("The College of Engineering is the second largest of the colleges at UC Berkeley.")
-    elif college == "Haas School of Business":
-        st.write("The Haas School of Business is one of the top business schools in the world.")
-    elif college == "School of Humanities and Sciences":
-        st.write("The School of Humanities and Sciences is the largest of the schools at Stanford University.")
-    elif college == "School of Engineering":
-        st.write("The School of Engineering is one of the top engineering schools in the world.")
-    elif college == "Graduate School of Business":
-        st.write("The Graduate School of Business is one of the top business schools in the world.")
+submitted = st.button(label="Submit")
 
-    # Display a link to the college's website
-    st.write(f"[Visit the {college} website](https://{college}.edu)")
+if submitted:
+    # Process user input and store it in Excel
+    worksheet.append([student_name, branch, email, selected_university, selected_college])
+    wb.save(r"c:\Users\mesaz\OneDrive\Desktop\STUDENT PORTAL.xlsx")
+
+
+    # Display success message
+    st.success("Student details submitted successfully!")
