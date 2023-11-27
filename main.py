4d2303a8a91a5c26412174f435833bed3836c9e5
mesasarah <102405106+mesasarah@users.noreply.github.com>
2023-11-27 16:52:46 +0530

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,26 @@
 import streamlit as st
 import openpyxl
+import requests
+
+def save_workbook(filename, url):
+    # Download the current Excel file from the cloud storage
+    response = requests.get(url)
+    with open('temp.xlsx', 'wb') as f:
+        f.write(response.content)
+
+    # Load the downloaded file into an openpyxl workbook
+    wb = openpyxl.load_workbook('temp.xlsx')
+    worksheet = wb.active
+
+    # Append user input to the worksheet
+    worksheet.append([student_name, branch, email, selected_university, selected_college])
+
+    # Save the updated workbook back to the cloud storage
+    wb.save('temp.xlsx')
+    response = requests.put(url, data=open('temp.xlsx', 'rb').read(), headers={'Content-Type': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'})
+
+    # Delete the temporary file
+    os.remove('temp.xlsx')
 
 
 # Initialize Excel workbook
@@ -33,7 +54,8 @@ submitted = st.button(label="Submit")
 if submitted:
     # Process user input and store it in Excel
     worksheet.append([student_name, branch, email, selected_university, selected_college])
-    wb.save(r"c:\Users\mesaz\OneDrive\Desktop\student.xlsx")
+    save_workbook(r"https://docs.google.com/spreadsheets/d/1Wv3fCL3PjdBNHZXFu4uu_t76fjeprwOd/edit?usp=drive_link&ouid=117306722142764576579&rtpof=true&sd=true", cloud_storage_url)
+
 
 
     # Display success message
